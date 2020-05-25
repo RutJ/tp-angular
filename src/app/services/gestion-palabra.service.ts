310c13c9363c1de1f3738f684700446ebc8e775f
unknown <jaquelinacruz.95@gmail.com>
2020-05-24 21:36:17 -0300

    Cree la carpeta images, agregue las imagenes necesarias

--- src/app/services/gestion-palabra.service.ts
+++ src/app/services/gestion-palabra.service.ts
@@ -5,15 +5,15 @@ import { Palabra } from '../models/palabra';
   providedIn: 'root'
 })
 export class GestionPalabraService {
-  animal1 = new Palabra(1,"CAT","gato","-----");
-  animal2 = new Palabra(1,"DOG","perro","-----");
-  animal3 = new Palabra(1,"LION","leon","-----");
-  navidad1 = new Palabra(2,"TREE","arbol","-----");
-  navidad2 = new Palabra(2,"GIFT","regalo","-----");
-  navidad3 = new Palabra(2,"STAR","estrella","-----");
-  utiles1 = new Palabra(3,"PEN","lapiz","-----");
-  utiles2 = new Palabra(3,"ERASER","borrador","-----");
-  utiles3 = new Palabra(3,"BACKPACK","mochila","-----");
+  animal1 = new Palabra(1,"CAT","gato","assets/images/gato.jpg");
+  animal2 = new Palabra(1,"DOG","perro","assets/images/perro.jpg");
+  animal3 = new Palabra(1,"LION","leon","assets/images/leon.jpg");
+  navidad1 = new Palabra(2,"TREE","arbol","assets/images/arbol.jpg");
+  navidad2 = new Palabra(2,"GIFT","regalo","assets/images/regalo.jpg");
+  navidad3 = new Palabra(2,"STAR","estrella","assets/images/estrella.jpg");
+  utiles1 = new Palabra(3,"PEN","lapiz","assets/images/lapiz.jpg");
+  utiles2 = new Palabra(3,"ERASER","borrador","assets/images/borrador.jpg");
+  utiles3 = new Palabra(3,"BACKPACK","mochila","assets/images/mochila.jpg");
   palabras = [this.animal1,this.animal2,this.animal3,this.navidad1,this.navidad2,this.navidad3,this.utiles1,this.utiles2,this.utiles3];
   constructor() {}
 
